3f1e3b501d335344805d362d3f35a57698f0fc95
Gloridust <Gloridust@gmail.com>
2023-09-25 20:10:26 +0800

    Update tk.py

--- tk.py
+++ tk.py
@@ -1,21 +1,48 @@
-import tkinter as tk
+import tkinter as tk    # 导入Tkinter库，用于创建GUI界面
+import random   # 导入random用于生成随机数
+
+# 生成随机数
+random_number = random.randint(0, 100)
+
+# 初始化GUI窗口
 root = tk.Tk()
 root.title("猜数字")
-root.geometry("500x250")
+root.geometry("300x150")
 
+# 创建标签，用于显示游戏标题
+title_label = tk.Label(root, text="猜数字小游戏")
+title_label.pack()
 
-import random
+# 创建输入框，用于接收用户输入
+input_entry = tk.Entry(root)
+input_entry.pack()
 
-random_number = random.randint(0, 100)  # 生成一个随机数，并将其存储在变量random_number中
-print("猜数字小游戏")
-input_number = int(input("输入一个整数："))
+# 创建按钮，用于提交用户的猜测
+submit_button = tk.Button(root, text="提交", command=lambda: check_guess())
+submit_button.pack()
 
-while input_number != random_number:
-    if input_number > random_number:
-        print("大了")
-    else:
-        print("小了")
-    
-    input_number = int(input("再次输入一个整数："))  # 读取用户的输入，并将其存储在input_number中
+# 创建标签，用于显示游戏结果
+result_label = tk.Label(root, text="")
+result_label.pack()
 
-print("Yes, you're right!")
+# 定义检查猜测的函数
+def check_guess():
+    user_guess = input_entry.get()  # 获取用户输入
+    try:
+        user_guess = int(user_guess)  # 尝试将用户输入转换为整数
+        if user_guess > random_number:
+            result_label.config(text="大了")
+        elif user_guess < random_number:
+            result_label.config(text="小了")
+        else:
+            result_label.config(text="Yes, you're right!")  # 用户猜中数字
+    except ValueError:
+        result_label.config(text="请输入一个整数")  # 处理用户输入非整数的情况
+
+# 运行GUI应用程序的主循环
+root.mainloop()
+
+# 建议：
+# 1. 如果你打算在GUI界面中实现这个游戏，可以考虑将用户输入的部分替换为GUI元素，而不是使用文本输入和打印消息。
+# 2. 对于Tkinter GUI，你可以创建一个输入框和一个文本标签来处理用户输入和显示消息，而不是使用命令行的input和print。
+# 3. 如果用户输入非整数，代码会引发异常。可以添加异常处理来处理这种情况，以确保程序不会崩溃。

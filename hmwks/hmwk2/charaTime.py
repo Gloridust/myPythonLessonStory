c08a3e8ac8d934338bd1e9453f72a89fb0a47e1d
Gloridust <Gloridust@gmail.com>
2023-10-13 11:39:07 +0800

    Update charaTime.py

--- hmwks/hmwk2/charaTime.py
+++ hmwks/hmwk2/charaTime.py
@@ -1,7 +1,8 @@
+# 输入两个字符串
 text = input("请输入要检查的文本字符串: ")
 characters = input("请输入要查找的字符集合: ")
 
-# 初始化字典用于次数统计
+# 初始化一个字典用于统计字符出现的次数
 char_count = {}
 
 # 遍历输入字符串中的每个字符
@@ -13,6 +14,10 @@ for char in text:
         else:
             char_count[char] = 1
 
-# 输出次数
-for char, count in char_count.items():
-    print(f"'{char}' 出现 {count} 次")
+# 统计字符出现次数之和
+total_count = 0
+for char in char_count:
+    total_count += char_count[char]
+
+# 输出字符集合中字符出现的总次数
+print(f"字符集合中字符出现的总次数为: {total_count}")

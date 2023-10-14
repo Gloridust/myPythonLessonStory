6fe0ba2f7995db8c608e90c016e82e6613132b25
Gloridust <Gloridust@gmail.com>
2023-10-14 17:14:04 +0800

    Update distance.py

--- hmwks/hmwk2/distance.py
+++ hmwks/hmwk2/distance.py
@@ -1,13 +1,9 @@
-# 输入第一个点的坐标
-x1 = input("请输入第一个点的 x 坐标: ")
-y1 = input("请输入第一个点的 y 坐标: ")
+import math
 
-# 输入第二个点的坐标
-x2 = input("请输入第二个点的 x 坐标: ")
-y2 = input("请输入第二个点的 y 坐标: ")
+x1 = float(input("请输入第一个点的 x 坐标: "))
+y1 = float(input("请输入第一个点的 y 坐标: "))
+x2 = float(input("请输入第二个点的 x 坐标: "))
+y2 = float(input("请输入第二个点的 y 坐标: "))
 
-# 计算曼哈顿距离
-distance = abs(x1 - x2) + abs(y1 - y2)
-
-# 输出距离
-print("两点之间的距离为:", distance)
+mdistance = abs(x1 - x2) + abs(y1 - y2)
+print("两点之间的曼哈顿距离为:", mdistance)

3b8967531f3c3ed19b4a08c730e4e328258d9fd0
Gloridust <Gloridust@gmail.com>
2023-11-26 15:01:41 +0800

    Create lst.py

---
+++ hmwks/huaqing/lst.py
@@ -0,0 +1,10 @@
+list = [2,6,8,0,4,9,3]
+i = 0
+
+while i < len(list):
+    for j in range(i+1, len(list)):
+        if list[j] < list[i]:
+            list[i], list[j] = list[j], list[i]  # 这里的代码看起来像是想交换两个元素的位置
+    i += 1
+
+print(list)

9f83a1c72cda7f3d83970818ffdd0fdd2cf051d7
Gloridust <Gloridust@gmail.com>
2023-12-17 00:37:07 +0800

    Update myBlog.py

--- reptile/myBlog.py
+++ reptile/myBlog.py
@@ -1,46 +1,34 @@
-import os
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+import os
 
-def get_article_links(blog_url):
-    """获取博客文章的链接列表"""
-    response = requests.get(blog_url)
+def get_article_links(main_page_url):
+    # Fetch the main page and extract article links
+    response = requests.get(main_page_url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    # 假设每篇文章的链接都在<a>标签内，您需要根据实际情况调整选择器
-    links = soup.find_all('a', class_='article-link')  # 修改class为您博客的相应class
-    return [urljoin(blog_url, link['href']) for link in links]
+    links = soup.find_all(...)  # Modify this to match your blog's structure
+    return [link['href'] for link in links]
 
-def save_article_content(url, folder_path):
-    """保存文章内容和图片"""
-    response = requests.get(url)
+def download_article_content_and_images(article_url):
+    response = requests.get(article_url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # 假设文章内容在一个特定的<div>标签内
-    article_content = soup.find('div', class_='article-content')  # 修改class为您博客的相应class
+    # Extract content
+    content = soup.find(...)  # Modify this based on your article's HTML structure
+    # Save content to a text file
 
-    # 保存文章文本
-    with open(os.path.join(folder_path, 'article.txt'), 'w', encoding='utf-8') as file:
-        file.write(article_content.text)
+    # Extract and download images
+    images = soup.find_all('img')
+    for img in images:
+        img_url = img['src']
+        # Code to download and save the image
 
-    # 保存文章内的图片
-    images = article_content.find_all('img')
-    for i, img in enumerate(images):
-        img_url = urljoin(url, img['src'])
-        img_data = requests.get(img_url).content
-        with open(os.path.join(folder_path, f'image_{i}.jpg'), 'wb') as file:
-            file.write(img_data)
-
-# 主函数
 def main():
-    blog_url = 'https://gloridust.xyz'  # 您博客的URL
-    article_links = get_article_links(blog_url)
+    main_page_url = 'https://yourblog.com'
+    article_links = get_article_links(main_page_url)
 
     for url in article_links:
-        folder_name = url.split('/')[-1]  # 假设文章的标题是URL的最后一部分
-        folder_path = os.path.join('articles', folder_name)
-        os.makedirs(folder_path, exist_ok=True)
-        save_article_content(url, folder_path)
+        download_article_content_and_images(url)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()

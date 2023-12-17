8181830491cc9f1f13400b7388d70ed116ca7b94
Gloridust <Gloridust@gmail.com>
2023-12-17 20:35:52 +0800

    注释

--- reptile/myBlog.py
+++ reptile/myBlog.py
@@ -1,35 +1,49 @@
+# 导入 requests 模块，用于执行 HTTP 请求
 import requests
+# 从 bs4 模块导入 BeautifulSoup，用于解析和操作 HTML
 from bs4 import BeautifulSoup
+# 导入 os 模块，通常用于操作文件系统
 import os
 
+# 定义一个函数来获取文章链接
 def get_article_links(main_page_url):
-    # Fetch the main page and extract article links
-    # response = requests.get(main_page_url)    修改请求代码，添加一个参数来禁用 SSL 验证
+    # 向指定的 URL 发送 HTTP GET 请求以获取主页内容。由于 SSL 验证被禁用，它不会验证 HTTPS 证书的合法性。
     response = requests.get(main_page_url, verify=False)
+    # 使用 BeautifulSoup 解析响应文本（HTML），使用 'html.parser' 作为解析器
     soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.find_all(...)  # Modify this to match your blog's structure
+    # 查找所有符合条件的链接（这里需要根据您博客的 HTML 结构进行修改）
+    links = soup.find_all(...)  
+    # 返回一个包含所有链接 'href' 属性的列表
     return [link['href'] for link in links]
 
+# 定义一个函数来下载文章内容和图片
 def download_article_content_and_images(article_url):
+    # 向文章的 URL 发送请求
     response = requests.get(article_url)
+    # 使用 BeautifulSoup 解析响应文本
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Extract content
-    content = soup.find(p)  # Modify this based on your article's HTML structure
-    # Save content to a text file
+    # 提取文章内容（需要根据您的文章 HTML 结构进行修改）
+    content = soup.find(p)  
+    # 将内容保存到文本文件的代码应放在这里
 
-    # Extract and download images
+    # 提取并下载图片
     images = soup.find_all('img')
     for img in images:
         img_url = img['src']
-        # Code to download and save the image
+        # 将图片下载并保存的代码应放在这里
 
+# 定义主函数
 def main():
+    # 定义要爬取的主页 URL
     main_page_url = 'https://gloridust.xyz'
+    # 获取文章链接
     article_links = get_article_links(main_page_url)
 
+    # 对于每个文章链接，下载其内容和图片
     for url in article_links:
         download_article_content_and_images(url)
 
-if __name__ == '__main__':
+# 如果此脚本是作为主程序运行，而不是被导入为模块，则执行 main 函数
+if __name__ == '__main__':  
     main()
--- reptile/practice1.py
+++ reptile/practice1.py
@@ -1,13 +1,20 @@
+# 安装依赖
 # pip install requests beautifulsoup4
 
-import requests
-from bs4 import BeautifulSoup
+# 导入所需的库
+import requests  # 引入 requests 库，用于向网站发送网络请求
+from bs4 import BeautifulSoup  # 引入 BeautifulSoup，用于解析和操作 HTML
 
-url = "https://www.ygeeker.com/"  # 用您想要爬取的网址替换
-response = requests.get(url)
+# 网络爬虫的目标 URL
+url = "https://www.ygeeker.com/"  # 定义变量 url，存储要爬取的网站地址
 
-soup = BeautifulSoup(response.text, 'html.parser')
+# 向目标网址发送 GET 请求
+response = requests.get(url)  # 使用 requests.get() 方法向指定的 URL 发送 GET 请求，并将响应对象存储在变量 response 中
 
-paragraphs = soup.find_all('div')
-for paragraph in paragraphs:
-    print(paragraph.text)
+# 解析网页内容
+soup = BeautifulSoup(response.text, 'html.parser')  # 使用 BeautifulSoup 解析 response 对象中的文本内容。这里 'html.parser' 是用于解析 HTML 的解析器
+
+# 查找并打印所有的 div 标签中的文本
+paragraphs = soup.find_all('div')  # 使用 BeautifulSoup 的 find_all 方法查找页面上所有的 <div> 标签，并将它们存储在变量 paragraphs 中
+for paragraph in paragraphs:  # 遍历 paragraphs 中的每一个元素（每一个 <div> 标签）
+    print(paragraph.text)  # 打印出 <div> 标签中的文本内容
